80d8832db2277ade46a113616739a43e55646508
Charlie Wang <charlie.wang@uwaterloo.ca>
2016-06-23 22:45:27 -0400

    fix runtest

--- test/runtest.sh
+++ test/runtest.sh
@@ -3,13 +3,18 @@
 # Smells like runscript from CS246
 
 failed=0
-path_prefix="${PWD}"/$(dirname "${2}")
+
+# absolute path to directory containing the config file
+path_prefix=$(realpath $(dirname "${2}"))
+# other test files are relative to $path_prefix
+
 while read stem; do
     OUTFILE=$(mktemp)
     EXPECTFILE="$path_prefix/expect/${stem}.expect"
-    "${PWD}/${1}" < "${path_prefix}/${stem}.mips" > "${OUTFILE}" 2>&1 
-    cmp "${OUTFILE}" "${EXPECTFILE}"
 
+    "${1}" < "${path_prefix}/${stem}.mips" > "${OUTFILE}" 2>&1 
+
+    cmp "${OUTFILE}" "${EXPECTFILE}"
     if [ "${?}" -ne 0 ]; then
         echo "Test ${stem} failed!"
         diff "${OUTFILE}" "${EXPECTFILE}"
