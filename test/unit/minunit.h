0889eefaacfbc62179cc91e4def6e7793b3a9b32
Charlie Wang <charliewanggib@gmail.com>
2016-06-24 22:04:44 -0400

    add documentation for minunit.h

--- test/unit/minunit.h
+++ test/unit/minunit.h
@@ -3,9 +3,14 @@
 // Note: flipped params of mu_assert
 
 
+// Return a message from the current function if the test fails
 #define mu_assert(test, message) do { current_test_name = __func__; if (!(test)) return message; } while (0)
+
+// Run a test, and return its message if it fails.
 #define mu_run_test(test) do { const char *message = test(); tests_run++; \
                                if (message != NULL) return message; } while (0)
+
+// Call after a test fails to print its name.
 #define mu_print_failing_test() do { printf("%s\n", current_test_name); } while (0)
 
 static int tests_run = 0;
