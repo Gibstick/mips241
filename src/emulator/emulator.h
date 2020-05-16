569838dd9b7ad07eeba0238ba8195fc022e1180a
Charlie Wang <Gibstick@users.noreply.github.com>
2020-05-15 21:52:56 -0400

    Fix alignment

--- src/emulator/emulator.h
+++ src/emulator/emulator.h
@@ -11,15 +11,14 @@
 
 
 // Load a program from a FILE into the machine at the offset.
-mips241_EXPORT void load_program(FILE *const file,
-                  Machine *const machine,
-                  uint32_t offset);
+mips241_EXPORT void load_program(FILE *const file, Machine *const machine, 
+                                 uint32_t offset);
 
 // Must call this function on startup to initialize things.
 mips241_EXPORT void init_emulator(void);
 
 // Dump memory to the file
 mips241_EXPORT void dump_memory(const Machine *const machine,
-                  const char *filename);
+                                const char *filename);
 
 #endif
